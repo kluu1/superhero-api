ea69489adf1f92884f187be262c8d87afafb6857
Kevin Luu <kevinluu@KLuu-CC.localdomain>
2020-10-01 17:10:48 -0400

    added endpoint to get all superheroes

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const port = 3000;
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('hello world')
+});
+
+app.get('/all', async (req, res) => {
+  try {
+    const request = await axios.get('https://akabab.github.io/superhero-api/api/all.json');
+    res.json(request.data);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 });
