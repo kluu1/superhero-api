6674fe02991ae663eaeadcc66bd4ed12e57d4b0a
Kevin Luu <kevinluu@KLuu-CC.localdomain>
2020-10-01 22:55:23 -0400

    deleted the file from git repo

--- build/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var express = require('express');
-var routes = require('./routes');
-var app = express();
-var port = 3000;
-app.use(routes);
-app.listen(port, function () {
-    console.log("Example app listening at http://localhost:" + port);
-});
--- build/routes/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var router = require('express').Router();
-var controller = require('../controllers');
-router.route('/all').get(controller.all);
-router.route('/id/:id').get(controller.id);
-module.exports = router;
