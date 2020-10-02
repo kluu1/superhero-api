5c4843e9f2d0b71dec9f34826474ab4bec055d71
Kevin Luu <kevinluu@KLuu-CC.localdomain>
2020-10-01 21:25:23 -0400

    fix conflicts

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.get('/all', async (req, res) => {
+  try {
+    const request = await axios.get('https://akabab.github.io/superhero-api/api/all.json');
+    res.json(request.data);
+  } catch (err) {
+    throw new Error('Error fetching superheroes..');
+  }
 });
 
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
